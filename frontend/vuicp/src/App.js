f76cf3cad75b238d0b77d75d024c37b1535cc413
PedroZambra <pezamesp@gmail.com>
2019-12-05 18:29:10 +0100

    starting react frontend, first screen

---
+++ frontend/vuicp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header.js';
+import LoginScreen from './components/LoginScreen.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <LoginScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/vuicp/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+        return(
+            <header>
+                <h2>VUI Croos Platform</h2>
+            </header>
+        )
+    }
+}
+
+export default Header;
