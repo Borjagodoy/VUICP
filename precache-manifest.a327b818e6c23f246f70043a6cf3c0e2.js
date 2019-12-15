5ecb6146814fe6deca3a2395ab18253a59a66e07
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-15 22:31:53 +0000

    Deploy Borjagodoy/VUICP to github.com/Borjagodoy/VUICP.git:gh-pages

---
+++ precache-manifest.a327b818e6c23f246f70043a6cf3c0e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bf51af70a63983fc38afd7b0792011",
+    "url": "/VUICP/index.html"
+  },
+  {
+    "revision": "9a27b667cb54d8dd8150",
+    "url": "/VUICP/static/css/main.a1aee1a5.chunk.css"
+  },
+  {
+    "revision": "c298f00f8b1ea28dbbea",
+    "url": "/VUICP/static/js/2.24bfb174.chunk.js"
+  },
+  {
+    "revision": "9a27b667cb54d8dd8150",
+    "url": "/VUICP/static/js/main.f252b5aa.chunk.js"
+  },
+  {
+    "revision": "c180cafce96e5cfe8fe6",
+    "url": "/VUICP/static/js/runtime-main.b96e30f9.js"
+  },
+  {
+    "revision": "d42efbc1a98e353bf85aeb38c9998c81",
+    "url": "/VUICP/static/media/background.d42efbc1.jpg"
+  },
+  {
+    "revision": "5a7058230cf2143a9844e491439d38f0",
+    "url": "/VUICP/static/media/example.5a705823.jpeg"
+  }
+]);
